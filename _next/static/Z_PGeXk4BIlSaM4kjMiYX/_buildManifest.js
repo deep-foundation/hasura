d23be911a9851cae715fb8fbd9dd5aea52ddfab0
Ivan S Glazunov <ivansglazunov@gmail.com>
2021-02-18 00:29:19 +0000

    initil gh-pages

---
+++ _next/static/Z_PGeXk4BIlSaM4kjMiYX/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-7efa8c872d437f50bfaf.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-059951b5ad6453b259bc.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
--- sql.tsx
+++
@@ -1,4 +0,0 @@
-export const sql = (strings: TemplateStringsArray, ...expr: any[]) =>
-  strings
-    .map((str, index) => str + (expr.length > index ? String(expr[index]) : ''))
-    .join('');
