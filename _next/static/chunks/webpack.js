610a7e64ee4a8abaf6a62eb94562269df3bed3e5
Ivan S Glazunov <ivansglazunov@gmail.com>
2021-05-15 22:10:14 +0000

    fix gh-pages

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "75e9de6756f19e07be1c";
+/******/ 	var hotCurrentHash = "3c49d0154c69bf541a3f";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
