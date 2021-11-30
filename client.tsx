5f79134f62264fbfe1f9be967d3f00a130125157
ivansglazunov <ivansglazunov@gmail.com>
2021-11-30 04:35:20 +0300

    0.0.6

--- client.tsx
+++ client.tsx
@@ -4,7 +4,11 @@ import { ApolloLink, concat, split } from 'apollo-link';
 import { WebSocketLink } from 'apollo-link-ws';
 import fetch from 'node-fetch';
 import path from 'path';
-import * as ws from 'ws';
+
+let ws;
+if (typeof(window) !== 'object') {
+  ws = require('ws');
+}
 
 const DEEP_FOUNDATION_HASURA_RELATIVE: boolean | undefined = ((r) => r ? !!+r : undefined)(process.env.DEEP_FOUNDATION_HASURA_RELATIVE);
 const NEXT_PUBLIC_DEEP_FOUNDATION_HASURA_RELATIVE: boolean | undefined = ((r) => r ? !!+r : undefined)(process.env.NEXT_PUBLIC_DEEP_FOUNDATION_HASURA_RELATIVE);
